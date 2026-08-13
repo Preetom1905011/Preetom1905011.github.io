2930e6e4db941aa71450be4cf1d4beb041c51cd2
Preetom1905011 <preetomicc@gmail.com>
2026-08-13 18:10:54 -0400

    feat: scroll reset when page nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 export function App() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <main className="site-main">
         <Outlet />
